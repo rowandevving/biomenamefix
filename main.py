4729abdf6b791cef8f1b20409d49be0a0d38c177
Willow <104677625+WillowDev@users.noreply.github.com>
2023-05-02 12:49:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def create_json_file(zip_file):
 
 def main():
     st.title("Biome Name Fix Generator")
-    st.write("You may have noticed that when you play with datapacks that add biomes e.g. Terralith, that mods like Xaero's minimap, Journeymap, MiniHUD and other mods that read biome names have untranslated strings, often looking something like 'biome.namespace.biomename', which is not all that pretty. This tool fixes that by generating a lang file that you can use in the /assets/minecraft/lang folder of a resourcepack or mod.")
+    st.write("You may have noticed that when you play with datapacks that add biomes e.g. Terralith, that mods like Xaero's minimap, Journeymap, MiniHUD and other mods that read biome names have untranslated strings, often looking something like 'biome.namespace.biomename', which is not all that pretty. This tool fixes that by generating a lang file that you can use in the /assets/minecraft/lang folder of a resourcepack or mod. If you wish to make fixes for multiple packs, you can use https://weld.smithed.dev to make a combined pack and then stick it back in here.")
     st.write("See this image as an example:")
     st.image('https://i.postimg.cc/YSZv9z51/bnf.png', use_column_width='always')
     file = st.file_uploader("Upload a datapack", type=["zip"])

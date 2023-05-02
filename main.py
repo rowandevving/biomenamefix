a39df48410f9e20c2d77f2f954b00ddd4f4acb65
Willow <104677625+WillowDev@users.noreply.github.com>
2023-05-02 11:43:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,6 @@ def create_json_file(zip_file):
 
 def main():
     st.title("Biome Name Fix Generator")
-    st.warning("This tool is a work in progress, various packs do not work. Please check back for updates!")
     st.write("You may have noticed that when you play with datapacks that add biomes e.g. Terralith, that mods like Xaero's minimap, Journeymap, MiniHUD and other mods that read biome names have untranslated strings, often looking something like 'biome.namespace.biomename', which is not all that pretty. This tool fixes that by generating a lang file that you can use in the /assets/minecraft/lang folder of a resourcepack or mod.")
     st.write("See this image as an example (from Stardust Labs Biome Name Fix)")
     st.image('https://cdn.modrinth.com/data/J9bjUrwA/images/69a3ba37029f391c689632bae06fca90307eff1e.png', use_column_width='always')

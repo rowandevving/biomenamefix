a2bb111ed5d717bb7aa942e63a15465bfd0322de
Willow <104677625+WillowDev@users.noreply.github.com>
2023-05-02 11:08:18 +0100

    updated main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import json
 import os
 import zipfile
@@ -5,16 +6,18 @@ import streamlit as st
 
 
 def check_for_biome_folders(zip_file):
-    with zipfile.ZipFile(zip_file, 'r') as myzip:
-        contents = myzip.namelist()
-        data_folders = [f for f in contents if f.startswith('data/') and f.endswith('/')]
-        biome_folders = []
-        for folder in data_folders:
-            folder_contents = myzip.namelist()
-            biome_folder = folder + "worldgen/biome/"
-            if biome_folder in folder_contents and "/tags/" not in folder:
-                biome_folders.append(biome_folder)
-        return biome_folders
+    with st.spinner("Please wait..."):
+        time.sleep(5)
+        with zipfile.ZipFile(zip_file, 'r') as myzip:
+            contents = myzip.namelist()
+            data_folders = [f for f in contents if f.startswith('data/') and f.endswith('/')]
+            biome_folders = []
+            for folder in data_folders:
+                folder_contents = myzip.namelist()
+                biome_folder = folder + "worldgen/biome/"
+                if biome_folder in folder_contents and "/tags/" not in folder:
+                    biome_folders.append(biome_folder)
+            return biome_folders
 
 
 def create_json_file(zip_file):
@@ -32,21 +35,23 @@ def create_json_file(zip_file):
 
 def main():
     st.title("Biome Name Fix Generator")
+    st.warning("This tool is a work in progress, various packs do not work. Please check back for updates!")
     st.write("You may have noticed that when you play with datapacks that add biomes e.g. Terralith, that mods like Xaero's minimap, Journeymap, MiniHUD and other mods that read biome names have untranslated strings, often looking something like 'biome.namespace.biomename', which is not all that pretty. This tool fixes that by generating a lang file that you can use in the /assets/minecraft/lang folder of a resourcepack or mod.")
+    st.write("See this image as an example (from Stardust Labs Biome Name Fix)")
+    st.image('https://cdn.modrinth.com/data/J9bjUrwA/images/69a3ba37029f391c689632bae06fca90307eff1e.png', use_column_width='always')
     file = st.file_uploader("Upload a datapack", type=["zip"])
     if file is not None:
         biome_folders = check_for_biome_folders(file)
         if len(biome_folders) > 0:
-            st.write("The following biome folders were found:")
+            st.info("The following biome folders were found:")
             for folder in biome_folders:
                 st.write(folder)
             create_json_file(file)
-            st.write("Download JSON file:")
             with open("en_us.json", "r") as infile:
                 json_data = json.load(infile)
-            st.download_button("Download", json.dumps(json_data, indent=2), file_name="en_us.json")
+            st.download_button("Download Lang JSON", json.dumps(json_data, indent=2), file_name="en_us.json")
         else:
-            st.write("No biome folders were found.")
+            st.error("No biome folders were found.")
 
 
 if __name__ == "__main__":
